a70e3f942f3850b86b35ec5899f4b12dbab53e90
MMosman <moustafamohamedoosman@gmail.com>
2026-05-07 22:31:38 +0300

    Change the compressing method to imagick instedof dg

--- app/Http/Traits/media.php
+++ app/Http/Traits/media.php
@@ -2,35 +2,36 @@
 
 namespace App\Http\Traits;
 
-// استدعاء كلاسات مكتبة الضغط
 use Intervention\Image\ImageManager;
-use Intervention\Image\Drivers\Gd\Driver;
+// استدعاء المحركين معاً فوق
+use Intervention\Image\Drivers\Gd\Driver as GdDriver;
+use Intervention\Image\Drivers\Imagick\Driver as ImagickDriver;
 
 trait media
 {
     public function uploadPhoto($img, $dir): string
     {
-        // 1. توليد اسم الصورة الجديد مع امتداد webp
         $imgName = uniqid() . '.webp';
-        
-        // 2. تحديد المسار الكامل للمجلد داخل الـ public
         $path = public_path("/dist/img/$dir/");
 
-        // 3. (خطوة أمان) التأكد من أن المجلد موجود فعلاً، وإذا لم يكن موجوداً يتم إنشاؤه
         if (!file_exists($path)) {
             mkdir($path, 0755, true);
         }
 
-        // 4. تهيئة مدير الصور
-        $manager = new ImageManager(new Driver());
+        // --- السحر هنا (اختيار المحرك تلقائياً) ---
+        // دالة extension_loaded بتفحص هل الإضافة موجودة في الـ PHP ولا لأ
+        if (extension_loaded('imagick')) {
+            $driver = new ImagickDriver();
+        } else {
+            $driver = new GdDriver();
+        }
 
-        // 5. قراءة الصورة المرفوعة في الذاكرة (الرام)
+        // تمرير المحرك المناسب للمدير
+        $manager = new ImageManager($driver);
         $image = $manager->read($img);
 
-        // 6. السحر هنا: حفظ الصورة في المسار الجديد، 
-        // المكتبة ستفهم تلقائياً من اسم الملف ($imgName) أنك تريد تحويلها لـ WebP
-        // وسيتم ضغط الجودة لـ 80% (مما يقلل الحجم جداً بدون التأثير على الشكل)
-        $image->save($path . $imgName, quality: 65);
+        // حفظ الصورة مع تقليل الجودة لـ 60
+        $image->save($path . $imgName, quality: 60);
 
         return $imgName;
     }

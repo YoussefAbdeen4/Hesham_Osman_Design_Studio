00a04a49f1e08f0e0a704c8bb1083b2aeb05993f
MMosman <moustafamohamedoosman@gmail.com>
2026-05-09 00:08:17 +0300

    fix search position

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('super.super')
 @section('title','Dashboard')
 @section('content')
-<div class="row">
+<div class="row dashboard">
     <!-- Products -->
     <div class="col-lg-3 col-6">
         <div class="small-box bg-warning">
